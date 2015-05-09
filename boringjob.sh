d8d865cc1bb886c9665a3659174298e3d5efd537
Luca Costa <zapduke@gmail.com>
2015-05-09 20:25:54 +0200

    Document script

--- boringjob.sh
+++ boringjob.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+#atm it reads prefs.json from modules/fetchers/indeed
+#and send you an email with the last 20 days job offers 
+#that match the keywords you specified in the json
+#see prefs-example.json
+#(you need an indeed publisher account)
+
 python modules/fetchers/indeed/indeed.py 
 
